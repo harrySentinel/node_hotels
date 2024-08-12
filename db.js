45c160f314ed5229bd43bab57f6fcee31c77d1c1
harrySentinel <adityasrivastav9721057380@gmail.com>
2024-08-12 19:56:54 +0530

    replacing ssl with tls: true

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 mongoose.connect(mongoURL, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
-    ssl: true
+    tls: true
 })
 
 // get the default connection
